8f6b062b60a7f51bb3942f32abc7c71e2eff62ce
asilem17 <mocan.melisa.maria@gmail.com>
2025-12-08 14:35:46 +0200

    Add project files.

---
+++ Mocan_Melisa_MariaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mocan_Melisa_MariaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
